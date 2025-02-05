35c866e2906d089fb3a078f6e6ac222ae8295b2f
Leandro Vivas <ianvivas12@gmail.com>
2025-02-05 19:51:23 +0800

    Modified Investor Modal

--- resources/views/components/add_investor_modal.blade.php
+++ resources/views/components/add_investor_modal.blade.php
@@ -1,7 +1,7 @@
 <div id="addClientModal" class="hidden fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-80">
     <div class="bg-white p-5 rounded-lg shadow-lg w-full max-w-5xl">
         <div class="flex justify-between items-center mb-6">
-            <h2 class="text-2xl font-semibold text-center fw-bold w-full">CLIENT PERSONAL INFORMATION</h2>
+            <h2 class="text-2xl font-semibold text-center fw-bold w-full">INVESTOR PERSONAL INFORMATION</h2>
             <button type="button" id="closeModal" class="text-gray-600 hover:text-gray-800">
                 <span class="text-2xl">&times;</span>
             </button>
@@ -38,49 +38,21 @@
                     
                     <div class="grid grid-cols-3 gap-4">
                         <div class="flex flex-col">
-                            <label for="age" class="text-gray-700 font-medium">Age</label>
-                            <input type="number" id="age" name="age" class="p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 w-full text-sm">
+                            <label for="contact_number" class="text-gray-700 font-medium">Contact Number</label>
+                            <input type="number" id="age" name="age" class="p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 w-full text-sm"  placeholder="+639*********">
                         </div>
                         <div class="flex flex-col ">
-                            <label for="birthday" class="text-gray-700 font-medium">Birthday</label>
-                            <input type="date" id="birthday" name="birthday" class="p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 w-full text-sm">
+                            <label for="amount_invest" class="text-gray-700 font-medium">Amount Invest</label>
+                            <input type="number" id="amount_invest" name="amount_invest" class="p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 w-full text-sm" placeholder="PHP">
                         </div>
-                        <div class="flex flex-col">
-                            <label for="gender" class="text-gray-700 font-medium">Gender</label>
-                            <select id="gender" name="gender" class="p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 w-full text-sm">
-                                <option value="male">Male</option>
-                                <option value="female">Female</option>
-                            </select>
-                        </div>
-                    </div>
-                    
-                    <div class="grid grid-cols-2 gap-4">
-                        <div class="flex flex-col">
-                            <label for="contact_number" class="text-gray-700 font-medium">Contact Number</label>
-                            <input type="tel" id="contact_number" name="contact_number" class="p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400">
-                        </div>
-                        <div class="flex flex-col">
-                            <label for="facebook" class="text-gray-700 font-medium">Facebook / Messenger</label>
-                            <input type="url" id="facebook" name="facebook" class="p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400">
-                        </div>
-                    </div>
-                    
-                    <p class="fs-5 fw-bold mt-4 mb-3">Contact Person</p>
-                    
-                    <div class="grid grid-cols-2 gap-4">
-                        
-                        <div class="flex flex-col">
-                            <label for="co-borrower" class="text-gray-700 font-medium">Co-Borrower</label>
-                            <input type="text" id="co-borrower" name="co-borrower" class="p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400">
-                        </div>
-                        <div class="flex flex-col">
-                            <label for="relationship_co-borrower" class="text-gray-700 font-medium">Relationship with Co-Borrower</label>
-                            <input type="text" id="relationship_co-borrower" name="relationship_co-borrower" class="p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400">
+                        <div class="flex flex-col ">
+                            <label for="percentage" class="text-gray-700 font-medium">Payment Percentage</label>
+                            <input type="number" id="percentage" name="percentage" class="p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 w-full text-sm"  placeholder="%">
                         </div>
                     </div>
                     
                     <div class="flex justify-end mt-4">
-                        <button type="button" class="next-btn bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-700 px-5">Next</button>
+                        <button type="button" class="next-btn bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-700 px-5">Submit</button>
                     </div>
                 </form>
             </div>       
--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -5,6 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Admin')</title>
     @vite('resources/css/app.css')
+    <style>
+        input[type=number]::-webkit-inner-spin-button, 
+        input[type=number]::-webkit-outer-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+        }
+
+        input[type=number] {
+            -moz-appearance: textfield;
+        }
+    </style>
 </head>
 <body class="bg-gray-100">
     <div class="flex h-screen">
